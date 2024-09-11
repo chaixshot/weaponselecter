09a6078bd58cc412ac5b8489a63f0c6dc823e9ab
chaixshot <chai5142xshot@gmail.com>
2024-09-11 23:05:09 +0700

    2.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,11 +3,18 @@ lua54 'yes'
 game 'gta5'
 -- use_fxv2_oal 'yes'
 
+version '2.0'
+
 client_scripts {
 	'config.lua',
 	'client/function.lua',
 	'client/quickslot.lua',
 	'client/weapon.lua',
+}
+
+server_scripts {
+	'config.lua',
+	'server/version.lua',
 }
 
 files {
@@ -19,7 +26,3 @@ files {
 }
 
 ui_page "html/index.html"
-
-escrow_ignore {
-	"config.lua",
-}
---
+++ server/version.lua
@@ -0,0 +1,18 @@
+---Version checker
+Citizen.CreateThread(function()
+	-- Citizen.Wait(2000)
+	local resourceName = GetCurrentResourceName()
+	local currentVersion = GetResourceMetadata(resourceName, "version", 0)
+	PerformHttpRequest("https://api.github.com/repos/chaixshot/weaponselecter/releases/latest", function (errorCode, resultData, resultHeaders)
+      if errorCode == 200 then
+			local data = json.decode(resultData)
+			if currentVersion ~= data.name then
+				print("------------------------------")
+				print("Update available for ^1"..resourceName.." ^3(current version: "..currentVersion..")^0")
+				print("Please update to the latest release ^2(version: "..data.name..")^0")
+				print("Check in ^3"..data.html_url.."^0")
+				print("------------------------------")
+			end
+		end
+	end)
+end)
